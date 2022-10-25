3557a2c9b76b309afa06a9992fbc245262338128
Aman Kumar <aman.kumar11112001@gmail.com>
2022-10-25 22:54:04 +0530

    SQLAlchemy setup done

--- Healthify/auth.py
+++ Healthify/auth.py
@@ -1,15 +1,35 @@
-from flask import Blueprint
+from crypt import methods
+from xmlrpc.client import boolean
+from flask import Blueprint, flash, render_template, request
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    return render_template("signin.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return "<h1>Logout</h1>"
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Signup</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash("Email must be greater than 4 characters.", category='error')
+        elif len(name) < 2:
+            flash("Name must be greater than 2 characters.", category='error')
+        elif password1 != password2:
+            flash('Password don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be greater than 7 characters', category='error')
+        else:
+            flash("Account created", category='success')
+            # add user to db 
+            pass
+    return render_template("signup.html")
