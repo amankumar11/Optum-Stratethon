10ff9b327f467b43a369707dd65531f07145b510
Aman Kumar <aman.kumar11112001@gmail.com>
2022-10-28 02:05:28 +0530

    form page ui done

--- Healthify/views.py
+++ Healthify/views.py
@@ -7,6 +7,10 @@ views = Blueprint('views',__name__)
 def home():
     return render_template("index.html")
 
+@views.route('/form')
+def form():
+    return render_template("predict_form.html")
+
 @views.route('/dashboard')
 @login_required
 def dashboard():
